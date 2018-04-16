e462007f3aeef302a38db2bca0512683e5206ba5
Aaron Faucher <aaron.faucher@gmail.com>
2018-04-16 17:46:04 -0400

    Modified to use only Unity Events UI component

--- Assets/Scripts/UIListener.cs
+++ Assets/Scripts/UIListener.cs
@@ -6,23 +6,9 @@ using Crayon;
 // TODO: This is a temp class for 4/16 that should be discarded
 public class UIListener : MonoBehaviour {
 
-	public SensorKnownEvents _eventToHear;
 
 	// Use this for initialization
 	void Start () {
-
-		// Subscribe to the event based on 
-		switch (_eventToHear) {
-		case SensorKnownEvents.Knocking:
-			SensorEventBroadcaster.instance.knockingEvent.AddListener (OnEvent);
-			break;
-		case SensorKnownEvents.Microwave:
-			SensorEventBroadcaster.instance.microwaveEvent.AddListener (OnEvent);
-			break;
-		case SensorKnownEvents.Door:
-			SensorEventBroadcaster.instance.doorEvent.AddListener (OnEvent);
-			break;
-		}
 
 	}
 	
@@ -31,9 +17,9 @@ public class UIListener : MonoBehaviour {
 		
 	}
 
-	void OnEvent() {
+	public void OnEvent(string thisEvent) {
 
-		Debug.Log (_eventToHear.ToString () + " heard by UIListener");
+		Debug.Log ("Event heard by UIListener");
 		this.gameObject.SetState (CrayonStateType.Custom, "activated");
 
 	}
