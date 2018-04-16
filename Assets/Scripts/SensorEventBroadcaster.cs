d15cb5dfbb92b320bcec277349aac171cfd50e29
Aaron Faucher <aaron.faucher@gmail.com>
2018-04-16 16:06:55 -0400

    Initial Commit

---
+++ Assets/Scripts/SensorEventBroadcaster.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Unity Event base that others can subscribe to
+/// </summary>
+[System.Serializable]
+public class SensorEvent : UnityEvent
+{
+}
+
+public class SensorEventBroadcaster : MonoBehaviour {
+
+	public static SensorEventBroadcaster instance;
+
+	private string _lastEvents;
+
+	// All sensor events
+	public SensorEvent knockingEvent;
+	public SensorEvent microwaveEvent;
+	public SensorEvent doorEvent;
+
+	void Awake() {
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnSensorEventsChanged(string eventsList){
+
+		// TODO: This needs to be changed to a loop to handle an array of events
+
+		// Check which events are new
+		string newEvent = CompareEvents(eventsList);
+
+		Debug.Log ("newEvent = " + newEvent + "; " + "lastEvent = " + _lastEvents);
+		BroadcastHandler (newEvent);
+
+
+	}
+
+	private string CompareEvents(string eventsList){
+
+		// TODO: This needs to be modified to handle an array of events
+
+		if (eventsList == _lastEvents) {
+			_lastEvents = eventsList;
+			return null;
+		} else {
+			_lastEvents = eventsList;
+			return eventsList;
+		}
+
+	}
+
+	private void BroadcastHandler(string eventsList)
+	{
+
+		switch (eventsList) {
+		case "knocking":
+			knockingEvent.Invoke();
+			break;
+		case "microwave":
+			microwaveEvent.Invoke();
+			break;
+		case "doorEvent":
+			doorEvent.Invoke();
+			break;
+		default:
+			Debug.Log ("Unrecognized event: " + eventsList);
+			break;
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/SensorListener.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+using SimpleJSON;
+
+public class SensorListener : MonoBehaviour {
+
+	public static SensorListener instance;
+
+	public string _urlBase = "http://localhost:8080/response.json";
+	public string[] _eventsToDetect = {"knocking","microwave","door"};
+	public float _refreshRate;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		StartCoroutine(SendRequest(_urlBase));
+
+	}
+		
+	private IEnumerator SendRequest(string requestString)
+	{
+		// Make the request
+		UnityWebRequest request = UnityWebRequest.Get(requestString);
+		yield return request.Send();
+
+		string result = request.downloadHandler.text;
+
+		// call function to Parse JSON
+		ParseJson(result);
+
+		yield return null;
+
+	}
+
+	// parse result
+	private void ParseJson(string data)
+	{
+		Debug.Log ("Response:");
+		Debug.Log(data);
+		// var N = SimpleJSON.JSON.Parse("[" + data + "]");
+		var N = SimpleJSON.JSON.Parse(data);
+
+		string sensorEvents = N["data"]["series"][0]["values"][0][0];
+		Debug.Log (sensorEvents);
+
+		// Send to the Sensor Broadcaster
+		SensorEventBroadcaster.instance.OnSensorEventsChanged (sensorEvents);
+
+	}
+
+
+}
+
