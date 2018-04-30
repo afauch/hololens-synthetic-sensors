5eb42fcc00aa04b53c5df14350defdc18fb3d09c
Aaron Faucher <aaron.faucher@gmail.com>
2018-04-30 17:37:55 -0400

    Incomplete status before adding SimpleHTTPServer

--- Assets/Scripts/Webhooks/ServerTest.cs
+++ Assets/Scripts/Webhooks/ServerTest.cs
@@ -1,271 +1,127 @@
-﻿using System;  
-using System.Collections.Generic;  
-using System.IO;  
-using System.Text;  
-using System.Threading;  
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
 using UnityEngine;
 
-#if !UNITY_EDITOR
-using System.Threading.Tasks;  
+#if UNITY_EDITOR
+using System.Net;
+using System.Net.Sockets;
 #endif
 
-public class ServerTest : MonoBehaviour {
+public class ServerTest : MonoBehaviour
+{
 
-	// Next up try this one:
+	// TODO: See this article on how to retrofit the sockets
+	// protocol into something that will work for HoloLens
 	// https://foxypanda.me/tcp-client-in-a-uwp-unity-app-on-hololens/
 
-	public string _host;
-	public string _port;
+	TcpListener server;
+	public int _port = 12345;
+	bool _run = true;
 
-	#if !UNITY_EDITOR
-	private bool _useUWP = true;
-	private Windows.Networking.Sockets.StreamSocket socket;
-	private Task exchangeTask;
-	#endif
+	int _debugCounter = 0;
 
-	#if UNITY_EDITOR
-	private bool _useUWP = false;
-	// TODO: Here, I would want it to be a server, not a client
-	System.Net.Sockets.TcpClient client;
-	System.Net.Sockets.NetworkStream stream;
-	private Thread exchangeThread;
-	#endif
-
-	private Byte[] bytes = new Byte[256];
-	private StreamWriter writer;
-	private StreamReader reader;
-
-	void Start() {
-
-		Connect (_host, _port);
-
-	}
-
-	public void Connect(string host, string port)
+	void Update ()
 	{
-		if (_useUWP)
-		{
-			ConnectUWP(host, port);
+		if (Input.GetKeyUp (KeyCode.S)) {
+			StartServer (_port);
 		}
-		else
-		{
-			ConnectUnity(host, port);
+			
+		if (Input.GetKeyUp (KeyCode.X)) {
+			StopServer ();
+		}
+
+		if (Input.GetKeyUp (KeyCode.T)) {
+			StartCoroutine (TestLoop());
 		}
 	}
 
-
-
-	#if UNITY_EDITOR
-	private void ConnectUWP(string host, string port)
-	#else
-	private async void ConnectUWP(string host, string port)
-	#endif
+	public void StartServer (int port)
 	{
-		#if UNITY_EDITOR
-		errorStatus = "UWP TCP client used in Unity!";
-		#else
-		try
-		{
-		if (exchangeTask != null) StopExchange();
+		_run = true;
+		UnityEngine.Debug.Log ("StartServer called");
+		IPAddress address = IPAddress.Parse ("127.0.0.1");
+		server = new System.Net.Sockets.TcpListener (address, _port);
+		server.Start ();
 
-		socket = new Windows.Networking.Sockets.StreamSocket();
-		Windows.Networking.HostName serverHost = new Windows.Networking.HostName(host);
-		await socket.ConnectAsync(serverHost, port);
+		StartCoroutine (Listen ());
 
-		Stream streamOut = socket.OutputStream.AsStreamForWrite();
-		writer = new StreamWriter(streamOut) { AutoFlush = true };
-
-		Stream streamIn = socket.InputStream.AsStreamForRead();
-		reader = new StreamReader(streamIn);
-
-		RestartExchange();
-		successStatus = "Connected!";
-		}
-		catch (Exception e)
-		{
-		errorStatus = e.ToString();
-		}
-		#endif
 	}
 
-	private void ConnectUnity(string host, string port)
+	public void StopServer ()
 	{
-		#if !UNITY_EDITOR
-		errorStatus = "Unity TCP client used in UWP!";
-		#else
-		try
-		{
-			if (exchangeThread != null) StopExchange();
-
-			client = new System.Net.Sockets.TcpClient(host, Int32.Parse(port));
-			stream = client.GetStream();
-			reader = new StreamReader(stream);
-			writer = new StreamWriter(stream) { AutoFlush = true };
-
-			RestartExchange();
-			successStatus = "Connected!";
-		}
-		catch (Exception e)
-		{
-			errorStatus = e.ToString();
-		}
-		#endif
+		_run = false;
 	}
 
-	private bool exchanging = false;
-	private bool exchangeStopRequested = false;
-	private string lastPacket = null;
-
-	private string errorStatus = null;
-	private string warningStatus = null;
-	private string successStatus = null;
-	private string unknownStatus = null;
-
-	public void RestartExchange()
+	IEnumerator TestLoop ()
 	{
-		#if UNITY_EDITOR
-		if (exchangeThread != null) StopExchange();
-		exchangeStopRequested = false;
-		exchangeThread = new System.Threading.Thread(ExchangePackets);
-		exchangeThread.Start();
-		#else
-		if (exchangeTask != null) StopExchange();
-		exchangeStopRequested = false;
-		exchangeTask = Task.Run(() => ExchangePackets());
-		#endif
-	}
-
-	public void Update()
-	{
-		if(lastPacket != null)
-		{
-			ReportDataToTrackingManager(lastPacket);
-		}
-
-		if(errorStatus != null)
-		{
-			Debug.LogError(errorStatus);
-			errorStatus = null;
-		}
-		if (warningStatus != null)
-		{
-			Debug.LogWarning(warningStatus);
-			warningStatus = null;
-		}
-		if (successStatus != null)
-		{
-			Debug.Log(successStatus);
-			successStatus = null;
-		}
-		if (unknownStatus != null)
-		{
-			Debug.Log(unknownStatus);
-			unknownStatus = null;
+		while (true == true) {
+			yield return new WaitForSeconds (1.0f);
+			UnityEngine.Debug.Log ("while test working");
 		}
 	}
 
-	public void ExchangePackets()
+
+	IEnumerator Listen ()
 	{
-		while (!exchangeStopRequested)
-		{
-			if (writer == null || reader == null) continue;
-			exchanging = true;
 
-			writer.Write("X\n");
-			Debug.Log("Sent data!");
-			string received = null;
+		// Buffer for reading data
+		byte[] bytes = new byte[1024];
+		string data;
 
-			#if UNITY_EDITOR
-			byte[] bytes = new byte[client.SendBufferSize];
-			int recv = 0;
-			while (true)
-			{
-				recv = stream.Read(bytes, 0, client.SendBufferSize);
-				received += Encoding.UTF8.GetString(bytes, 0, recv);
-				if (received.EndsWith("\n")) break;
+		//Enter the listening loop
+		while (_run) {
+
+			UnityEngine.Debug.Log (_debugCounter);
+
+			Console.Write ("Waiting for a connection... ");
+
+			// Perform a blocking call to accept requests.
+			// You could also user server.AcceptSocket() here.
+			TcpClient client = server.AcceptTcpClient ();
+			Console.WriteLine ("Connected!");
+
+			// Get a stream object for reading and writing
+			NetworkStream stream = client.GetStream ();
+
+			int i;
+
+			// Loop to receive all the data sent by the client.
+			i = stream.Read (bytes, 0, bytes.Length);
+
+			while (i != 0) {
+				// Translate data bytes to a ASCII string.
+				data = System.Text.Encoding.ASCII.GetString (bytes, 0, i);
+				Console.WriteLine (String.Format ("Received: {0}", data));
+
+				// Process the data sent by the client.
+				data = data.ToUpper ();
+
+				byte[] msg = System.Text.Encoding.ASCII.GetBytes (data);
+
+				// Send back a response.
+				stream.Write (msg, 0, msg.Length);
+				Console.WriteLine (String.Format ("Sent: {0}", data));
+
+				i = stream.Read (bytes, 0, bytes.Length);
+				yield return new WaitForSeconds(1.0f);
+
 			}
-			#else
-			received = reader.ReadLine();
-			#endif
 
-			lastPacket = received;
-			Debug.Log("Read data: " + received);
+			// Shutdown and end connection
+			client.Close ();
 
-			exchanging = false;
-		}
-	}
+			_debugCounter++;
+			yield return new WaitForSeconds(1.0f);
 
-	private void ReportDataToTrackingManager(string data)
-	{
-		if (data == null)
-		{
-			Debug.Log("Received a frame but data was null");
-			return;
 		}
 
-		var parts = data.Split(';');
-		foreach(var part in parts)
-		{
-			ReportStringToTrackingManager(part);
-		}
+		yield return new WaitForSeconds(1.0f);
+
 	}
-
-	private void ReportStringToTrackingManager(string rigidBodyString)
-	{
-		var parts = rigidBodyString.Split(':');
-		var positionData = parts[1].Split(',');
-		var rotationData = parts[2].Split(',');
-
-		int id = Int32.Parse(parts[0]);
-		float x = float.Parse(positionData[0]);
-		float y = float.Parse(positionData[1]);
-		float z = float.Parse(positionData[2]);
-		float qx = float.Parse(rotationData[0]);
-		float qy = float.Parse(rotationData[1]);
-		float qz = float.Parse(rotationData[2]);
-		float qw = float.Parse(rotationData[3]);
-
-		Vector3 position = new Vector3(x, y, z);
-		Quaternion rotation = new Quaternion(qx, qy, qz, qw);
-
-		// TrackingManager.UpdateRigidBodyData(id, position, rotation);
-	}
-
-	public void StopExchange()
-	{
-		exchangeStopRequested = true;
-
-		#if UNITY_EDITOR
-		if (exchangeThread != null)
-		{
-			exchangeThread.Abort();
-			stream.Close();
-			client.Close();
-			writer.Close();
-			reader.Close();
-
-			stream = null;
-			exchangeThread = null;
-		}
-		#else
-		if (exchangeTask != null) {
-		exchangeTask.Wait();
-		socket.Dispose();
-		writer.Dispose();
-		reader.Dispose();
-
-		socket = null;
-		exchangeTask = null;
-		}
-		#endif
-		writer = null;
-		reader = null;
-	}
-
-	public void OnDestroy()
-	{
-		StopExchange();
-	}
-
 
 }
