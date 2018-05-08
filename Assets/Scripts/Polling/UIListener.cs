c21074a0f01d834e7edb285de5c6bd3f9e6f655b
Aaron Faucher <aaron.faucher@gmail.com>
2018-05-08 09:00:39 -0400

    Reorganized

---
+++ Assets/Scripts/Polling/UIListener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Crayon;
+
+// TODO: This is a temp class for 4/16 that should be discarded
+public class UIListener : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnEvent(string thisEvent) {
+
+		Debug.Log (thisEvent + " event heard by UIListener");
+		this.gameObject.SetState (CrayonStateType.Custom, "activated");
+
+	}
+	
+
+}
